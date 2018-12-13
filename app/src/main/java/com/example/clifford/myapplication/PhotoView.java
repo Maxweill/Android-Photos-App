712cb1467d0a5f95e13dd7e5e14720dc30ec0894
Max <maxwell.mucha@gmail.com>
2018-12-13 18:24:56 -0500

    Images left

--- app/src/main/java/com/example/clifford/myapplication/PhotoView.java
+++ app/src/main/java/com/example/clifford/myapplication/PhotoView.java
@@ -1,12 +1,16 @@
 package com.example.clifford.myapplication;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -34,7 +38,7 @@ public class PhotoView extends AppCompatActivity {
         a = globalVariable.getAlbum();
         x = intent.getIntExtra("index",0);
 
-        Photo p = a.getPhotos().get(x);
+        final Photo p = a.getPhotos().get(x);
         p.bmap = BitmapFactory.decodeByteArray(p.bytemap, 0, p.bytemap.length);
 
         imageView = (ImageView)findViewById(R.id.imageView);
@@ -62,6 +66,16 @@ public class PhotoView extends AppCompatActivity {
         listView.setAdapter(adapter);
 
         //add Tag
+
+        Button deletetag = (Button) findViewById(R.id.deletetag);
+        deletetag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Photo photo = a.getPhotos().get(x);
+                showDeleteTagDialog(PhotoView.this, photo);
+            }
+        });
+
         addTag.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -92,6 +106,18 @@ public class PhotoView extends AppCompatActivity {
                 }
 
 
+                String [] tags = new String[p.getTags().size()];
+                int i = 0;
+                for ( Tag t: p.getTags() ){
+
+                    tags[i] = t.toString();
+                    i++;
+                }
+
+                ListAdapter adapter = new ArrayAdapter<String>(PhotoView.this, android.R.layout.simple_expandable_list_item_1, tags);
+
+                listView.setAdapter(adapter);
+
 
 
             }
@@ -103,34 +129,36 @@ public class PhotoView extends AppCompatActivity {
 
                 x--;
 
+                Photo photo;
+
                 if ( x < 0 ) {
 
                     x = a.getPhotos().size()-1;
-                    Photo p = a.getPhotos().get(x);
-                    p.bmap = BitmapFactory.decodeByteArray(p.bytemap, 0, p.bytemap.length);
-                    imageView.setImageBitmap(p.bmap);
+                    photo = a.getPhotos().get(x);
+                    photo.bmap = BitmapFactory.decodeByteArray(photo.bytemap, 0, photo.bytemap.length);
+                    imageView.setImageBitmap(photo.bmap);
 
                 }
 
                 else{
 
-                    Photo p = a.getPhotos().get(x);
-                    p.bmap = BitmapFactory.decodeByteArray(p.bytemap, 0, p.bytemap.length);
-                    imageView.setImageBitmap(p.bmap);
+                    photo = a.getPhotos().get(x);
+                    photo.bmap = BitmapFactory.decodeByteArray(photo.bytemap, 0, photo.bytemap.length);
+                    imageView.setImageBitmap(photo.bmap);
 
                 }
 
-      /*          String [] tags = new String[p.getTags().size()];
+                String [] tags = new String[photo.getTags().size()];
                 int i = 0;
-                for ( Tag t: p.getTags() ){
+                for ( Tag t: photo.getTags() ){
 
                     tags[i] = t.toString();
                     i++;
                 }
 
-                ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, tags);
+                ListAdapter adapter = new ArrayAdapter<String>(PhotoView.this, android.R.layout.simple_expandable_list_item_1, tags);
 
-                listView.setAdapter(adapter); */
+                listView.setAdapter(adapter);
 
             }
         });
@@ -140,25 +168,80 @@ public class PhotoView extends AppCompatActivity {
             public void onClick(View v) {
                 x++;
 
+                Photo photo;
+
                 if ( x == a.getPhotos().size()){
 
                     x = 0;
-                    Photo p = a.getPhotos().get(x);
-                    p.bmap = BitmapFactory.decodeByteArray(p.bytemap, 0, p.bytemap.length);
-                    imageView.setImageBitmap(p.bmap);
+                    photo = a.getPhotos().get(x);
+                    photo.bmap = BitmapFactory.decodeByteArray(photo.bytemap, 0, photo.bytemap.length);
+                    imageView.setImageBitmap(photo.bmap);
                 }
 
                 else {
 
-                    Photo p = a.getPhotos().get(x);
-                    p.bmap = BitmapFactory.decodeByteArray(p.bytemap, 0, p.bytemap.length);
-                    imageView.setImageBitmap(p.bmap);
+                    photo = a.getPhotos().get(x);
+                    photo.bmap = BitmapFactory.decodeByteArray(photo.bytemap, 0, photo.bytemap.length);
+                    imageView.setImageBitmap(photo.bmap);
 
                 }
+
+                String [] tags = new String[photo.getTags().size()];
+                int i = 0;
+                for ( Tag t: photo.getTags() ){
+
+                    tags[i] = t.toString();
+                    i++;
+                }
+
+                ListAdapter adapter = new ArrayAdapter<String>(PhotoView.this, android.R.layout.simple_expandable_list_item_1, tags);
+
+                listView.setAdapter(adapter);
 
             }
         });
 
+
+
+    }
+
+    private void showDeleteTagDialog(Context c, final Photo pho){
+        final EditText taskEditText = new EditText(c);
+        AlertDialog dialog = new AlertDialog.Builder(c)
+                .setTitle("Delete which tag?")
+                .setView(taskEditText)
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String task = String.valueOf(taskEditText.getText());
+                        for(Tag t: pho.tags)
+                        {
+                            if (t.name.equals(task))
+                            {
+                                pho.deleteTag(task);
+                                String [] tags = new String[pho.getTags().size()];
+                                int i = 0;
+                                for ( Tag tag: pho.getTags() ){
+
+                                    tags[i] = tag.toString();
+                                    i++;
+                                }
+
+                                ListAdapter adapter = new ArrayAdapter<String>(PhotoView.this, android.R.layout.simple_expandable_list_item_1, tags);
+
+                                listView.setAdapter(adapter);
+
+                                return;
+                            }
+                        }
+                        Toast.makeText(PhotoView.this, "Invalid tag type, valid tags are 'location' and 'person'.", Toast.LENGTH_LONG).show();
+
+
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create();
+        dialog.show();
 
 
     }
